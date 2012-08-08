7bfd565e20c6876eabba48eaa331805b7fbb69e0
Nick Farina <nfarina@gmail.com>
2012-08-08 14:40:30 -0700

    Initial Commit

---
+++ CalloutViewSamples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalloutViewSamples
+//
+//  Created by Nick Farina on 8/8/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CalloutViewSamples/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CalloutViewSamples
+//
+//  Created by Nick Farina on 8/8/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
