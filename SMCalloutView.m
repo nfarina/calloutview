065961e85aa8ea05afb48a0708a083f5c1de4e2d
PJ Vea <pj.vea@busybusy.com>
2019-04-01 18:29:54 -0600

    Updated container height logic.

--- SMCalloutView.m
+++ SMCalloutView.m
@@ -197,8 +197,14 @@ NSTimeInterval const kSMCalloutViewRepositionDelayForUIScrollView = 1.0/3.0;
 - (CGFloat)calloutContainerHeight {
     if (self.contentView)
         return self.contentView.frameHeight + self.contentViewInset.bottom + self.contentViewInset.top;
-    else if (self.subtitleView || self.subtitle.length > 0)
+    else if (self.subtitleView || self.subtitle.length > 0) 
+    {
+        if (self.subtitleView.frameHeight == 0)
+        {
+            return CALLOUT_SUB_DEFAULT_CONTAINER_HEIGHT;
+        }
         return CALLOUT_SUB_DEFAULT_CONTAINER_HEIGHT + self.subtitleView.frameHeight - 15;
+    }        
     else
         return CALLOUT_DEFAULT_CONTAINER_HEIGHT;
 }
